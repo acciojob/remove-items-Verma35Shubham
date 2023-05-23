5e9c13bb4ac29f8e62c901c0d56c715f749d3f74
Shubham Verma <verma.shubham2537@gmail.com>
2023-05-23 10:36:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+ document.getElementById('removeButton').addEventListener('click', function() {
+  var colorSelect = document.getElementById('colorSelect');
+  var selectedColor = colorSelect.value; // Get the selected color value
+
+  // Find the option with the selected color value and remove it
+  for (var i = 0; i < colorSelect.options.length; i++) {
+    if (colorSelect.options[i].value === selectedColor) {
+      colorSelect.remove(i);
+      break;
+    }
+  }
+});
